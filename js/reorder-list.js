406df2f5bec9c73a303cddbf5318544497ce9265
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-28 16:21:50 +0200

    changing the reorder function and adding another eventlistener to clone the best movie

--- js/reorder-list.js
+++ js/reorder-list.js
@@ -1,13 +1,28 @@
 export function reorder(e) {
   let list = document.querySelector("ul");
-  let movie = list.querySelectorAll("li");
+  let movieList = list.querySelectorAll("li");
+  let bestMovie = list.firstChild;
+  if (e.keyCode === 68) {
+    let bestMovieClone = bestMovie.cloneNode(true);
+    list.prepend(bestMovieClone);
+  }
   if (e.keyCode === 82) {
     let i = 0;
-    while (i < movie.length) {
-      list.appendChild(
-        movie[Math.floor(Math.random() * (movie.length - 1)) + 1]
-      );
-      i++;
+    while (i < movieList.length) {
+      let randomNumber = getRandomNumber();
+      let movie = movieList[randomNumber];
+      if (movie.classList.contains("important")) {
+        list.prepend(movie);
+      } else {
+        list.appendChild(movie);
+        i++;
+      }
     }
   }
+}
+function getRandomNumber() {
+  let list = document.querySelector("ul");
+  let movieList = list.querySelectorAll("li");
+  let randomNumber = Math.floor(Math.random() * (movieList.length - 1));
+  return randomNumber;
 }
